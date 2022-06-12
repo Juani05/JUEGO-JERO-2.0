4eb330a403682e8c798aed58ca9a4e31893b810e
Juani05 <102042211+Juani05@users.noreply.github.com>
2022-06-12 12:40:56 -0300

    Initial commit

---
+++ Assets/Scripts/collision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public GameObject txt_perdiste;
+    public AudioManager audioManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Auto")
+        {
+            audioManager.playclip();
+            Destroy(gameObject);
+            txt_perdiste.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# JUEGO-JERO-main
+ 
